82eb5a38cfdcb98c9bac9281b452a288d045a271
Denis Lozhkin <dlozhkin@xbsoftware.com>
2021-06-20 11:33:51 +0300

    Finish with moves validation

--- Shared/Board.fs
+++ Shared/Board.fs
@@ -5,9 +5,9 @@ open Utils
 
 module GamePlay =
     type Column = | A | B | C | D | E | F
-        with static member Index col = [A;B;C;D;E;F]
+        with static member Index col = List.findIndex (fun x -> x = col) [A;B;C;D;E;F]
     type Row = | One | Two | Three | Four
-        with static member List = [One; Two; Three; Four]
+        with static member Index row = List.findIndex (fun x -> x = row) [One; Two; Three; Four]
     type Cell = { Col: Column; Row: Row }
 
     type PlayerSide = | Odd | Even
@@ -23,9 +23,24 @@ module GamePlay =
 
     let private isFreeCell (board: Board) cell = board.[cell].IsNone
 
-    let private isNeighbourCell (originCell: Cell) (targetCell: Cell) = true
+    let private isOccupiedCell (board: Board) cell = board.[cell].IsSome
+
+    let private isNeighbourCell (originCell: Cell) (targetCell: Cell) = 
+        Math.Abs(Column.Index originCell.Col - Column.Index targetCell.Col) <= 1
+        &&
+        Math.Abs(Row.Index originCell.Row - Row.Index targetCell.Row) <= 1
+
+    let private isEnemyCard (originGameCard: GameCard) (targetGameCard: GameCard) = originGameCard.Side <> targetGameCard.Side 
 
     let private cellsList board = Map.toList board |> List.map fst
+
+    let private enemiesCellsList (board: Board) (gameCard: GameCard) = 
+        cellsList board 
+            |> List.filter ( fun cell -> 
+                            match board.[cell] with
+                                | None -> false
+                                | Some targetGameCard -> isEnemyCard gameCard targetGameCard  
+                   )
 
     let ValidMoves (moveType: MoveType) originCell (board: Board) =
         match board.[originCell] with
@@ -34,6 +49,10 @@ module GamePlay =
             match moveType with
             | Reposition ->
                 match gameCard.Card.Unit.Feature with
-                    | Flier _ -> cellsList board |> List.filter (isFreeCell board) 
-                    | Walker _ -> cellsList board |> List.filter (isNeighbourCell originCell <&> isFreeCell board)
-
+                    | Flier _ -> cellsList board |> List.filter (isFreeCell board) // Every free cell is available
+                    | Walker _ | Shooter _ -> cellsList board |> List.filter (isNeighbourCell originCell <&> isFreeCell board) // Cell is free and is next to current
+            | Attack ->
+                match gameCard.Card.Unit.Feature with
+                    | Shooter _ -> enemiesCellsList board gameCard // Every enemy on the board
+                    | Walker _ | Flier _ -> enemiesCellsList board gameCard // Enemies that are on the neihbour cell
+                                                |> List.filter (isNeighbourCell originCell)
