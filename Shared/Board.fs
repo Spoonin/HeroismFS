42f22151cfc0385e4110e5b3aef99147772ab9fd
Denis Lozhkin <dlozhkin@xbsoftware.com>
2021-07-27 09:15:27 +0300

    Empty Board

--- Shared/Board.fs
+++ Shared/Board.fs
@@ -4,10 +4,11 @@ open Cards
 
 module Boards =
     type Column = | A | B | C | D | E | F
-        with static member Index col = List.findIndex (fun x -> x = col) [A;B;C;D;E;F]
+        with static member Index col = Seq.findIndex (fun x -> x = col) (Utils.UnionCasesSeq<Column> ())
     type Row = | One | Two | Three | Four
-        with static member Index row = List.findIndex (fun x -> x = row) [One; Two; Three; Four]
+        with static member Index row = Seq.findIndex (fun x -> x = row) (Utils.UnionCasesSeq<Row> ())
     type Cell = { Col: Column; Row: Row }
+
 
     type PlayerSide = | Odd | Even
 
@@ -17,7 +18,7 @@ module Boards =
     }
 
     type Board = Map<Cell, GameCard option>
-
-    type BoardStatus = { EvenReady: bool; OddReady: bool }
-
     
+    let Empty: Board = Seq.allPairs (Utils.UnionCasesSeq<Column> ()) (Utils.UnionCasesSeq<Row> ()) 
+                        |> Seq.map (fun colRowPair -> ({Col = fst colRowPair; Row = snd colRowPair}, None))
+                        |> Map.ofSeq
--- Shared/Utils.fs
+++ Shared/Utils.fs
@@ -1,3 +1,10 @@
 namespace SharedModels
+    open Microsoft.FSharp.Reflection
+   
     module Utils =
         let (<&>) f g = (fun x -> f x && g x)
+
+
+        let UnionCasesSeq<'T>() =
+            FSharpType.GetUnionCases(typeof<'T>)
+            |> Seq.map (fun x -> FSharpValue.MakeUnion(x, Array.zeroCreate(x.GetFields().Length)) :?> 'T)
