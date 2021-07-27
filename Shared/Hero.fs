ad0a8c3ef32d7ad5026ce7d9142dec07a662ca0f
Denis Lozhkin <dlozhkin@xbsoftware.com>
2021-07-27 09:15:38 +0300

    Refactoring

--- Shared/Hero.fs
+++ Shared/Hero.fs
@@ -4,13 +4,23 @@ open Boards
 
 module Heroes = 
 
-    type HeroId = string
+    type HeroId = HeroId of string
 
     type Hero = {
         Id: HeroId
         Name: string
         PictureSrc: string
         Level: uint
-        CurrentGame: (Game * PlayerSide)  option
+        CurrentGame: Game  option
         UnitPool: Map<Unit, uint>
     }
+
+    let hero id name pictureSrc level currentGame unitPool = 
+        {
+            Id = id
+            Name = name
+            PictureSrc = pictureSrc
+            Level = level
+            CurrentGame = currentGame
+            UnitPool = unitPool
+        }
